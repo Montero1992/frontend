28dfa0965583fa48a59284aece14f68d37c17451
jmontero <jmdeamuedo@gmail.com>
2022-09-25 10:27:19 +0200

    test

---
+++ routes/api.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+const https = require('https');
+
+https.get('https://jmonterobackend.azurewebsites.net/api', (resp) => {
+  let data = '';
+
+  // A chunk of data has been received.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data).explanation);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
+
+module.exports = router;
+
+
