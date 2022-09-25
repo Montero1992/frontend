6d3389edbb83c0c6ae234f2d4cfa576f92d63b95
jmontero <jmdeamuedo@gmail.com>
2022-09-25 10:36:55 +0200

    test

--- routes/api.js
+++ routes/api.js
@@ -1,23 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const https = require('https');
 
-router.get('https://jmonterobackend.azurewebsites.net/api', (resp) => {
+https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
   let data = '';
 
   // A chunk of data has been received.
-  router.on('data', (chunk) => {
+  resp.on('data', (chunk) => {
     data += chunk;
   });
 
   // The whole response has been received. Print out the result.
-  router.on('end', () => {
+  resp.on('end', () => {
     console.log(JSON.parse(data).explanation);
   });
 
 }).on("error", (err) => {
   console.log("Error: " + err.message);
 });
-
-module.exports = router;
 
 
